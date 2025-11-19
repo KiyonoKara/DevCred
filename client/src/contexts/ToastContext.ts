1f867101de36e25af3fb2c3b8ea7cb1d41e738aa
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2025-11-18 20:47:07 -0500

    the attempted notif system, tried several ways, not working

--- client/src/contexts/ToastContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-import { Toast } from '../components/notifications/ToastNotification';
-
-export interface ToastContextType {
-  showToast: (message: string, options?: Partial<Toast>) => void;
-  dismissToast: (id: string) => void;
-}
-
-export const ToastContext = createContext<ToastContextType | undefined>(undefined);
