204f312407bddadf09197963cc60ade35d31d9f3
zindagikamra <kamra.z@northeastern.edu>
2025-11-23 06:56:58 -0500

    fixed type mismatch for added points field

--- shared/types/user.d.ts
+++ shared/types/user.d.ts
@@ -36,7 +36,7 @@ export interface User extends UserCredentials {
   activityHistory?: string[];
   activeResumeId?: string;
   notificationPreferences?: NotificationPreferences;
-  points: Number;
+  points: number;
 }
 
 /**
